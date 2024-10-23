3d28adfba0024c705cfb606024aae3dd07ffc0a9
Mervin Fansler <mervin.fansler@bric.ku.dk>
2024-10-23 10:09:04 +0200

    edit build_prefix in cross-compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+set -x
 
 ## manual fix for https://github.com/conda-forge/r-base-feedstock/issues/349
 if [[ "$target_platform" == "linux-aarch64" || "$target_platform" == "linux-ppc64le" || "$target_platform" == "osx-arm64" ]]; then
-    MAKECONF="$PREFIX/lib/R/etc/Makeconf"
-
+    if [[ "$build_platform" != "$target_platform" ]]; then
+        MAKECONF="${BUILD_PREFIX}/lib/R/etc/Makeconf"
+    else
+        MAKECONF="${PREFIX}/lib/R/etc/Makeconf"
+    fi
+    
     # piggyback CC17 and C17FLAGS on generic definition
-    sed -i 's|^CC17 =.*|CC17 = $(CC) -std=gnu17|' "$MAKECONF"
-    sed -i 's|^C17FLAGS =.*|C17FLAGS = $(CFLAGS)|' "$MAKECONF"
+    sed -i 's|^CC17 =.*|CC17 = $(CC) -std=gnu17|' "${MAKECONF}"
+    sed -i 's|^C17FLAGS =.*|C17FLAGS = $(CFLAGS)|' "${MAKECONF}"
 fi
 
 export DISABLE_AUTOBREW=1
