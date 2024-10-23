bbc25ca39aa3536c70a21290b642a1bc0a402391
Mervin Fansler <mervin.fansler@bric.ku.dk>
2024-10-23 09:27:30 +0200

    manual set flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+## manual fix for https://github.com/conda-forge/r-base-feedstock/issues/349
+export CC17="${CC} -std=gnu17"
+export CC17FLAGS="${CFLAGS}"
+
 export DISABLE_AUTOBREW=1
 
 # shellcheck disable=SC2086
